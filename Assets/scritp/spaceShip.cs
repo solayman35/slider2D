4d750faa52f9a76c44d6d3215efc73bf8f63eb24
Solayman <solayman.bubt@gmail.com>
2019-03-07 18:50:40 +0600

    First commit

---
+++ Assets/scritp/spaceShip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceShip : MonoBehaviour 
+{
+	
+	public float speed = .1f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed, 0f, 0f);
+		
+	}
+
+	public void OnCollision2D(Collision2D collision) {
+		Destroy(gameObject);
+	}
+}
